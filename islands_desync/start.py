69a777bf4212eedc1dda74686418c2bfa6974e87
superiorshrimp <szymon.zychowicz300701@gmail.com>
2024-04-11 19:57:01 +0200

    na dzisiaj tyle

--- islands_desync/start.py
+++ islands_desync/start.py
@@ -2,6 +2,7 @@ import json
 import sys
 from datetime import datetime
 import os
+os.environ["RAY_DEDUP_LOGS"] = "0"
 import ray
 from islands.core.IslandRunner import IslandRunner
 from islands.selectAlgorithm import RandomSelect
@@ -13,39 +14,39 @@ from geneticAlgorithm.run_hpc.run_algorithm_params import (
 from islands.topologies.TorusTopology import TorusTopology
 from islands.topologies.CompleteTopology import CompleteTopology
 
-os.environ["RAY_DEDUP_LOGS"] = "0"
 
 def main():
-    pass
     # if sys.argv[2] != " ":
     #     ray.init(_temp_dir=sys.argv[2])
 
-    # params = RunAlgorithmParams(
-    #     island_count=int(sys.argv[1]),
-    #     number_of_emigrants=int(sys.argv[3]),
-    #     migration_interval=int(sys.argv[4]),
-    #     dda=sys.argv[5],
-    #     tta=sys.argv[6],
-    #     series_number=1,
-    # )
+    params = RunAlgorithmParams(
+        island_count=int(sys.argv[1]),
+        number_of_emigrants=int(sys.argv[3]),
+        migration_interval=int(sys.argv[4]),
+        dda=sys.argv[5],
+        tta=sys.argv[6],
+        series_number=1,
+    )
 
-    # computation_refs = IslandRunner(TorusTopology, RandomSelect, params).create()
-    # #computation_refs = IslandRunner(RingTopology, RandomSelect, params).create()
-    # #computation_refs = IslandRunner(CompleteTopology, RandomSelect, params).create()
+    computation_refs = IslandRunner(TorusTopology, RandomSelect, params).create()
+    #computation_refs = IslandRunner(CompleteTopology, RandomSelect, params).create()
 
-    # results = ray.get(computation_refs)
+    results = ray.get(computation_refs)
 
-    # iterations = {result["island"]: result for result in results}
+    iterations = {result["island"]: result for result in results}
 
-    # with open(
-    #     "logs/"
-    #     + "iterations_per_second"
-    #     + datetime.now().strftime("%m-%d-%Y_%H%M")
-    #     + ".json",
-    #     "w",
-    # ) as f:
-    #     json.dump(iterations, f)
+    with open(
+        "logs/"
+        + "iterations_per_second"
+        + datetime.now().strftime("%m-%d-%Y_%H%M")
+        + ".json",
+        "w",
+    ) as f:
+        json.dump(iterations, f)
 
 
 if __name__ == "__main__":
+    ray.init(address="127.0.0.1:6379")
+    assert ray.is_initialized()
+    
     main()
