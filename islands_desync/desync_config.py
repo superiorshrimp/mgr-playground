fae1f215b3cc7b280e14f485a6a3a8cc6683bf7e
superiorshrimp <szymon.zychowicz300701@gmail.com>
2024-12-21 16:29:54 +0100

    config

---
+++ islands_desync/desync_config.py
@@ -0,0 +1,40 @@
+import json
+import os
+
+PATH = './islands_desync/geneticAlgorithm/algorithm/configurations/algorithm_configuration.json'
+
+def ring(delay, n):
+    T = [
+        [delay if abs(i-j) == 1 else -1 for i in range(n)] for j in range(n)
+    ]
+
+    T[0][-1] = delay
+    T[-1][0] = delay
+
+    return T
+
+def print_array(T):
+    for i in range(len(T)):
+        for j in range(len(T[i])):
+            print(T[i][j], end=" ")
+        print()
+
+def save(T):
+    with open(PATH, 'r') as f:
+        data = json.load(f)
+        data['island_delays'] = {
+            str(i): T[i] for i in range(len(T))
+        }
+    print(data)
+
+    os.remove(PATH)
+
+    with open(PATH, 'w+') as f:
+        json.dump(data, f, indent=2)
+
+if __name__ == "__main__":
+    delay = 10
+    n = 10
+    T = ring(delay, n)
+    print_array(T)
+    save(T)
