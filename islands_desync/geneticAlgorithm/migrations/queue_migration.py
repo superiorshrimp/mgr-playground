c7a37e857091189961ee0497cf08b4c064067f17
superiorshrimp <szymon.zychowicz300701@gmail.com>
2024-12-22 14:26:18 +0100

    a

--- islands_desync/geneticAlgorithm/migrations/queue_migration.py
+++ islands_desync/geneticAlgorithm/migrations/queue_migration.py
@@ -21,7 +21,7 @@ class QueueMigration(Migration):
         for i in individuals_to_migrate:
             destination = random.choice(
                 [
-                    i for i in self.rabbitmq_delays[str(self.island)] if i != -1
+                    i for i in range(len(self.rabbitmq_delays[str(self.island)])) if self.rabbitmq_delays[str(self.island)][i] != -1
                 ]
             )
 
