01a7ea1d1082aa9f6581187f1200cd95fd4d9b45
superiorshrimp <szymon.zychowicz300701@gmail.com>
2025-07-01 19:46:40 +0200

    a

--- islands_desync/geneticAlgorithm/migrations/queue_migration.py
+++ islands_desync/geneticAlgorithm/migrations/queue_migration.py
@@ -23,8 +23,11 @@ class QueueMigration(Migration):
         self, individuals_to_migrate, iteration_number, island_number, timestamp, island
     ):
         island_relevant_data = None
-        if not isinstance(self.emigration.select_algorithm, RandomSelect) and not self.send_everywhere():  # TODO: refactor maybe for 2 more parent classes?
-            island_relevant_data = ray.get(self.emigration.select_algorithm.get_island_relevant_data(self.emigration.islands))
+        emigration_select_algorithm = self.emigration.select_algorithm
+        if random.random() < self.emigration.select_algo_coef:
+            self.emigration.select_algorithm = RandomSelect()
+            if not isinstance(self.emigration.select_algorithm, RandomSelect) and not self.send_everywhere():  # TODO: refactor maybe for 2 more parent classes?
+                island_relevant_data = ray.get(self.emigration.select_algorithm.get_island_relevant_data(self.emigration.islands))
 
         for i, ind in enumerate(individuals_to_migrate):
 
@@ -48,6 +51,7 @@ class QueueMigration(Migration):
                     routing_key=f"island-from-{self.island}-to-{destination}",
                     body=json.dumps(data),
                 )
+                self.emigration.select_algorithm = emigration_select_algorithm
 
     def receive_individuals(
         self, step_num: int, evaluations: int
